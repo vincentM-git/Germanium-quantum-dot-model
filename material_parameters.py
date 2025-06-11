0dcf11beda90bac5e816f46f8bef4a3801157caf
vincentM-git <frenchvince.vm@gmail.com>
2025-06-11 11:13:30 +0200

    Delete material_parameters.py

--- material_parameters.py
+++
@@ -1,9 +0,0 @@
-#Luttinger and strain parameters gamma1 gamma2 gamma3 kappa qu bv[meV] nu=2*c_12/c_11 Ck[meV.nm] C4[meV.nm]/ materials Si Ge InP GaAs InAs InSb
-parameters = [
-    {"Material": "Si", "ga1": 4.285, "ga2": 0.339, "ga3": 1.446, "ka": -0.42, "qu": 0.01, "bv": -2.1e3, "nu": 0.77},
-    {"Material": "Ge", "ga1": 13.38, "ga2": 4.24, "ga3": 5.69, "ka": 3.41, "qu": 0.06, "bv": -2.86e3, "nu": 0.73},
-    {"Material": "InP", "ga1": 4.95, "ga2": 1.65, "ga3": 2.35, "ka": 0.97, "qu": 0},
-    {"Material": "GaAs", "ga1": 6.85, "ga2": 2.1, "ga3": 2.9, "ka": 1.2, "qu": 0.01},
-    {"Material": "InAs", "ga1": 20.4, "ga2": 8.3, "ga3": 9.1, "ka": 7.6, "qu": 0.04, "bv":-1.8e3, "Ck": -1.12, "C4": 7e2},
-    {"Material": "InSb", "ga1": 37.1, "ga2": 16.5, "ga3": 17.7, "ka": 15.6, "qu": 0.39},
-]
